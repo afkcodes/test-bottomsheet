f1afef72a4775c7b22cdb5d58e44d18391e7dfa7
Ashish Kumar <ashish.kmr@outlook.com>
2022-01-20 23:29:51 +0530

    initial commit

---
+++ src/config/config.ts
@@ -0,0 +1,14 @@
+export const FONTS = {
+  regular: 'Gilroy-Regular',
+  medium: 'Gilroy-Medium',
+  semiB: 'Gilroy-Semibold',
+  bold: 'Gilroy-Bold',
+  sizeXS: 8,
+  sizeS: 12,
+  sizeR: 16,
+  sizeXR: 20,
+  sizeM: 24,
+  sizeXM: 28,
+  sizeL: 32,
+  sizeXL: 36,
+};
